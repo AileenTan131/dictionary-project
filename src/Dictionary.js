e9a2df7ea236d938ec7e7402fa803d1d5b454968
Minim131 <100144145+Minim131@users.noreply.github.com>
2022-07-21 12:15:07 +0800

    Created default keyword to run whenever page loads

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,17 +4,27 @@ import Results from "./Results";
 
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleDictResponse(response) {
     setResults(response.data[0]);
+    setLoaded(true);
+    //3. when loaded is true, the form gets run
   }
+
+  function search() {
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    //2. api call runs the default keyword
+    axios.get(apiURL).then(handleDictResponse);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiURL).then(handleDictResponse);
+    search();
+    //Note: Search() section cannot be integrated to the else section, for handleSubmit requires api call too.
   }
 
   function updateKeyword(event) {
@@ -22,19 +32,23 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="What would you like to search for?"
-          autoFocus={true}
-          onChange={updateKeyword}
-          // style="width:200px"
-          size="80"
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="What would you like to search for?"
+            autoFocus={true}
+            onChange={updateKeyword}
+            size="80"
+          />
+        </form>
+        <Results results={results} />
+      </div> //4. onsubmit, user input gets send to api (onSubmit={handleSubmit})
+      //5. onChange={updateKeyword} replaces default keyword with setKeyword state
+    );
+  } else {
+    search(); //1. when page not loaded, i want page to run the api call
+  }
 }
