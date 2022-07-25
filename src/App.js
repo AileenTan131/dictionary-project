d5c4d3d03a413946937f7782763e8b495367d7f7
Minim131 <100144145+Minim131@users.noreply.github.com>
2022-07-25 18:21:22 +0800

    changed keyword

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <Dictionary defaultKeyword="food" />
+          <Dictionary defaultKeyword="welcome" />
         </header>
         <footer className="App-footer">
           <small>
