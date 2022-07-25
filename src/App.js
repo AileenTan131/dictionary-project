130846935a57f0674255bc11eb0e0ed4180a2782
Minim131 <100144145+Minim131@users.noreply.github.com>
2022-07-25 17:59:53 +0800

    Updated footer texts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,23 @@ export default function App() {
           <Dictionary defaultKeyword="food" />
         </header>
         <footer className="App-footer">
-          <small>Coded by Aileen Tan</small>
+          <small>
+            Coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/aileen-tan-878157b5/"
+              className="footer-links"
+            >
+              Aileen Tan
+            </a>
+            ,{" "}
+            <a
+              href="https://github.com/Minim131/dictionary-project"
+              className="footer-links"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and hosted on Netlify
+          </small>
         </footer>
       </div>
     </div>
